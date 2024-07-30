d24fcad89eb0cf726861f48dbf760080d1d76977
KostadinkaDyuzova <dyuzova.kostadinka@gmail.com>
2024-07-30 10:53:59 +0100

    font, color, size,style,alt

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, JavaScript! I am an alert box");
+function My_First_Function() {
+var String = "Hello, I am Bulgarian!";
+var result = String.fontcolor ("blue");
+document.getElementById("Bulgarian").innerHTML =
+result;
+}
